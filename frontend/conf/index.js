ac06cb695abac74a84b96caeea5e68685aad7e70
CrioUser <crio-user@criodo.com>
2023-03-05 21:07:22 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "http://13.127.54.33:8085" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -3,10 +3,12 @@ import config from "../conf/index.js";
 async function init() {
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
-
+  console.log(cities)
   //Updates the DOM with the cities
   cities.forEach((key) => {
+    
     addCityToDOM(key.id, key.city, key.description, key.image);
+    
   });
 }
 
@@ -14,6 +16,18 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
+  try{
+  let result = await fetch(`${config.backendEndpoint}/cities`)
+  
+  let data= result.json()
+  
+  return data
+  }
+  catch(err){
+    return null
+  }
+  
+  
 
 }
 
@@ -21,6 +35,25 @@ async function fetchCities() {
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let pointing = document.getElementById("data")
+  
+  pointing.innerHTML+= `<div class="col-lg-3 col-md-6 col-sm-12">
+                                <div class="tile card">
+                                  <a id = ${id} href="pages/adventures/?city="+"${id}">
+                                  <img class="card-img" src=${image} alt="img">
+                                  <div class="kkk card-img-overlay">
+                                    <h4 >${city}</h4>
+                                    <p >${description}</p>
+                                </div>
+                                  
+                                </a>
+                                </div>
+                                
+                              </div>`
+   
+    // return pointing
+
+
 
 }
 

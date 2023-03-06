d3340fc2ec9ddb4f50624197925b1da7c5a94d53
CrioUser <crio-user@criodo.com>
2023-03-06 13:28:56 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://13.127.54.33:8085" };
+const config = { backendEndpoint: "http://3.110.5.162:8082" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -39,7 +39,7 @@ function addCityToDOM(id, city, description, image) {
   
   pointing.innerHTML+= `<div class="col-lg-3 col-md-6 col-sm-12">
                                 <div class="tile card">
-                                  <a id = ${id} href="pages/adventures/?city="+"${id}">
+                                  <a id = ${id} href="pages/adventures/?city=${id}">
                                   <img class="card-img" src=${image} alt="img">
                                   <div class="kkk card-img-overlay">
                                     <h4 >${city}</h4>
